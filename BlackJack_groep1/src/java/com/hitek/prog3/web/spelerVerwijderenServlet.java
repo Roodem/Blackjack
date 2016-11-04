a2a8c22c0fe4667b352a5858fa80f8ecdfb3c7b5
tahsi <tahsin_mughal@hotmail.com>
2016-11-05 00:34:18 +0100

    aanpassing van model en afwerking van opties pagina's

--- BlackJack_groep1/src/java/com/hitek/prog3/web/spelerVerwijderenServlet.java
+++ BlackJack_groep1/src/java/com/hitek/prog3/web/spelerVerwijderenServlet.java
@@ -7,15 +7,11 @@ package com.hitek.prog3.web;
 
 import com.hitek.prog3.db.service.PlayerService;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.Player;
 
 /**
  *
