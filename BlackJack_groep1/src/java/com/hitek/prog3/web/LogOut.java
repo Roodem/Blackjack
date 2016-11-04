ff463d133d263e47c673e84607a9101463270484
Roodem <robindemeulenaere@hotmail.com>
2016-11-04 13:07:59 +0100

    update inlog

--- BlackJack_groep1/src/java/com/hitek/prog3/web/LogOut.java
+++ BlackJack_groep1/src/java/com/hitek/prog3/web/LogOut.java
@@ -50,7 +50,7 @@ public class LogOut extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-      
+     
     }
 
     /**
